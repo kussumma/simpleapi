3da667bab1ff412a814b48a01c8d32331e5edc8d
Kusuma J <sensnerd@gmail.com>
2022-08-24 11:30:53 +0800

    finish

--- main.go
+++ main.go
@@ -4,13 +4,24 @@ import (
 	"log"
 	"net/http"
 
+	"simpleapi/controllers"
 	"simpleapi/initializers"
+	"simpleapi/routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 var (
-	server *gin.Engine
+	server              *gin.Engine
+	AuthController      controllers.AuthController
+	AuthRouteController routes.AuthRouteController
+
+	UserController      controllers.UserController
+	UserRouteController routes.UserRouteController
+
+	PostController      controllers.PostController
+	PostRouteController routes.PostRouteController
 )
 
 func init() {
@@ -21,6 +32,15 @@ func init() {
 
 	initializers.ConnectDB(&config)
 
+	AuthController = controllers.NewAuthController(initializers.DB)
+	AuthRouteController = routes.NewAuthRouteController(AuthController)
+
+	UserController = controllers.NewUserController(initializers.DB)
+	UserRouteController = routes.NewRouteUserController(UserController)
+
+	PostController = controllers.NewPostController(initializers.DB)
+	PostRouteController = routes.NewRoutePostController(PostController)
+
 	server = gin.Default()
 }
 
@@ -30,11 +50,20 @@ func main() {
 		log.Fatal("🚀 Could not load environment variables", err)
 	}
 
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowOrigins = []string{"http://localhost:8000", config.ClientOrigin}
+	corsConfig.AllowCredentials = true
+
+	server.Use(cors.New(corsConfig))
+
 	router := server.Group("/api")
 	router.GET("/healthchecker", func(ctx *gin.Context) {
 		message := "Welcome to Golang with Gorm and Postgres"
 		ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": message})
 	})
 
+	AuthRouteController.AuthRoute(router)
+	UserRouteController.UserRoute(router)
+	PostRouteController.PostRoute(router)
 	log.Fatal(server.Run(":" + config.ServerPort))
 }
